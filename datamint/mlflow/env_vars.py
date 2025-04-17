9f39241bd521fa8ed366740b784af54887547324
Lucashsmello <lucashsmello@gmail.com>
2025-04-17 17:08:56 -0300

    Enhance project handling by adding support for project name retrieval and error handling in get_active_project_id

--- datamint/mlflow/env_vars.py
+++ datamint/mlflow/env_vars.py
@@ -2,3 +2,4 @@ from enum import Enum
 
 class EnvVars(Enum):
     DATAMINT_PROJECT_ID = "DATAMINT_PROJECT_ID"
+    DATAMINT_PROJECT_NAME = "DATAMINT_PROJECT_NAME"
--- datamint/mlflow/tracking/fluent.py
+++ datamint/mlflow/tracking/fluent.py
@@ -17,24 +17,45 @@ def get_active_project_id() -> str | None:
     """
     Get the active project ID from the environment variable or the global variable.
     """
+    global _ACTIVE_PROJECT_ID
+
+    if _ACTIVE_PROJECT_ID is not None:
+        return _ACTIVE_PROJECT_ID
     # Check if the environment variable is set
     project_id = os.getenv(EnvVars.DATAMINT_PROJECT_ID.value)
     if project_id is not None:
+        _ACTIVE_PROJECT_ID = project_id
         return project_id
+    project_name = os.getenv(EnvVars.DATAMINT_PROJECT_NAME.value)
+    if project_name is not None:
+        project = _find_project_by_name(project_name)
+        if project is not None:
+            _ACTIVE_PROJECT_ID = project['id']
+            return _ACTIVE_PROJECT_ID
 
-    # If not set, return the global variable
-    return _ACTIVE_PROJECT_ID
+    return None
+
+
+def _find_project_by_name(project_name: str) -> Optional[dict]:
+    dt_client = APIHandler(check_connection=False)
+    project = dt_client.get_project_by_name(project_name)
+    if project is None:
+        raise DatamintException(f"Project with name '{project_name}' does not exist.")
+    if 'error' in project:
+        raise DatamintException(f'Error getting project: {project["error"]}')
+    return project
+
 
 def set_project(project_name: Optional[str] = None, project_id: Optional[str] = None) -> dict:
+    global _ACTIVE_PROJECT_ID
     if project_name is None and project_id is None:
         raise MlflowException("You must specify either a project name or a project id")
 
     if project_name is not None and project_id is not None:
         raise MlflowException("You cannot specify both a project name and a project id")
 
-    dt_client = APIHandler(check_connection=False)
-
     with _PROJECT_LOCK:
+        dt_client = APIHandler(check_connection=False)
         if project_id is None:
             project = dt_client.get_project_by_name(project_name)
             if project is None:
@@ -47,8 +68,7 @@ def set_project(project_name: Optional[str] = None, project_id: Optional[str] = 
             if project is None:
                 raise DatamintException(f"Project with id '{project_id}' does not exist.")
 
-    global _ACTIVE_PROJECT_ID
-    _ACTIVE_PROJECT_ID = project_id
+        _ACTIVE_PROJECT_ID = project_id
 
     # Set 'DATAMINT_PROJECT_ID' environment variable
     # so that subprocess can inherit it.
