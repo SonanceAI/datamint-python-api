a1a24ac4e0a1a5b4e8a58857eba1ebadf82e866f
Lucashsmello <lucashsmello@gmail.com>
2025-07-29 09:30:48 -0300

    Fixed --verbose

--- datamint/client_cmd_tools/datamint_upload.py
+++ datamint/client_cmd_tools/datamint_upload.py
@@ -463,8 +463,9 @@ def _parse_args() -> tuple[Any, list[str], Optional[list[dict]], Optional[list[s
 
     if args.verbose:
         # Get the console handler and set to debug
+        print(logging.getLogger().handlers)
         logging.getLogger().handlers[0].setLevel(logging.DEBUG)
-        logging.getLogger('datamint').handlers[0].setLevel(logging.DEBUG)
+        logging.getLogger('datamint').setLevel(logging.DEBUG)
         _LOGGER.setLevel(logging.DEBUG)
         _USER_LOGGER.setLevel(logging.DEBUG)
 
