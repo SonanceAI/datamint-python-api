8b00bf7f8b9155df433eb818bafcbcb360930910
Lucashsmello <lucashsmello@gmail.com>
2026-01-22 11:18:01 -0300

    Fixed mlflow artifact_path not supplied when logging a datamint model

--- datamint/mlflow/flavors/datamint_flavor.py
+++ datamint/mlflow/flavors/datamint_flavor.py
@@ -116,6 +116,7 @@ def log_model(
     **kwargs
 ):
     return Model.log(
+        artifact_path=None,
         datamint_model=datamint_model,
         supported_modes=supported_modes,
         name=name,
